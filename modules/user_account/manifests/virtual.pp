861fff4d1a3fbaf157d30fcd1785acab58ac4562
Chris Rode <cirode@gmail.com>
2012-02-13 00:33:54 +1100

    moved user stuff into manifests directory

--- modules/user_account/manifests/virtual.pp
+++ modules/user_account/manifests/virtual.pp
@@ -1,4 +1,4 @@
-class user_account::virtual() {
+class user_account::virtual {
   @user_account{'cirode':
     uid => '100',
     key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDoYvJZ55HhTPlfglAay/p3q0JDyGEcVbfVzoG5Knx+Vt6N/rq8lacsxnlP/IzlaZZIucvUhq8OK/hsqKztegZRbESCcX526brKqq1JTmpC+OdG7j1Le2FkFIv42d9qolWhEizyGmCQLSSil6Ij65etAnZnLP98HNk7dDOxF1kWjHm7kmGqLUtRtMrH+m5lAEcCZ2RvRw6Treqn8L6QX5EWql97e1Mr+5geObPQ63hfBIMGZ/G+LWiygNy4vwKfL1OmSoQaQXZ7Q7AdSCVyRDQJNCHlJdkrHbcEACJBWZmpGymdhONGmQ2m442Q6R2fzMXbbIgAu/RNt4lz6Vo5kET/'
