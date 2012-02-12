980d6a2afe112a04ea05229d2956cdb224758d49
Chris Rode <cirode@gmail.com>
2012-02-12 22:27:10 +1100

    passed gid not uid

--- server.pp
+++ server.pp
@@ -32,7 +32,7 @@ define user_account (
 
 
 user_account{'cirode':
-  gid => '100',
+  uid => '100',
   key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDoYvJZ55HhTPlfglAay/p3q0JDyGEcVbfVzoG5Knx+Vt6N/rq8lacsxnlP/IzlaZZIucvUhq8OK/hsqKztegZRbESCcX526brKqq1JTmpC+OdG7j1Le2FkFIv42d9qolWhEizyGmCQLSSil6Ij65etAnZnLP98HNk7dDOxF1kWjHm7kmGqLUtRtMrH+m5lAEcCZ2RvRw6Treqn8L6QX5EWql97e1Mr+5geObPQ63hfBIMGZ/G+LWiygNy4vwKfL1OmSoQaQXZ7Q7AdSCVyRDQJNCHlJdkrHbcEACJBWZmpGymdhONGmQ2m442Q6R2fzMXbbIgAu/RNt4lz6Vo5kET/'
 }
 
