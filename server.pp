d0b58e6fd1a7ae63849b735d0924d85598a7c7a2
Chris Rode <cirode@gmail.com>
2012-02-12 20:34:08 +1100

    ssh_authorized_key

--- server.pp
+++ server.pp
@@ -12,7 +12,7 @@ user { "cirode":
 	managehome => true,
 }
 
-ssh_authorised_key{'cirode_public_key':
+ssh_authorized_key{'cirode_public_key':
   ensure => present,
   key => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEHr7keRZxIqrnl/H9euyyNyN/jTYZc7u25qyCOd6bzbm8T0lUNdtlfgz7piCRqrcwLbKIFltmqZAT6MribPsMbY1DqnYMRo4eWSoSGycUyK6X65O1inIPCz+Xus5FL6m1AIpQBVNoNfpTTBGuF8UwKkwt0yG2PaPB3OCsNKhzVHtnINk9y7EbaRkX4QtbG/vI0UbenxnXu2E/CO0rlXllO1p/OGqTLSsrXNyI5elkb//5NXwVQ44yh5RwWab7AeVKJ+41qxBZwzU88t0wwkvl7eJtcFULWNYbz6qpki/X1Z4GGoS0jhkGi8+k/1LyUhI6Rr5FQTjFhfswfsSiA0Bb cirode@Charon.local',
   user => 'cirode',
