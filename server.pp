7a4de5fb5837f322634a32e00b4c1eb8385496e1
Chris Rode <cirode@gmail.com>
2012-02-12 20:42:20 +1100

    changed key

--- server.pp
+++ server.pp
@@ -14,7 +14,7 @@ user { "cirode":
 
 ssh_authorized_key{'cirode_public_key':
   ensure => present,
-  key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDEHr7keRZxIqrnl/H9euyyNyN/jTYZc7u25qyCOd6bzbm8T0lUNdtlfgz7piCRqrcwLbKIFltmqZAT6MribPsMbY1DqnYMRo4eWSoSGycUyK6X65O1inIPCz+Xus5FL6m1AIpQBVNoNfpTTBGuF8UwKkwt0yG2PaPB3OCsNKhzVHtnINk9y7EbaRkX4QtbG/vI0UbenxnXu2E/CO0rlXllO1p/OGqTLSsrXNyI5elkb//5NXwVQ44yh5RwWab7AeVKJ+41qxBZwzU88t0wwkvl7eJtcFULWNYbz6qpki/X1Z4GGoS0jhkGi8+k/1LyUhI6Rr5FQTjFhfswfsSiA0Bb',
+  key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDoYvJZ55HhTPlfglAay/p3q0JDyGEcVbfVzoG5Knx+Vt6N/rq8lacsxnlP/IzlaZZIucvUhq8OK/hsqKztegZRbESCcX526brKqq1JTmpC+OdG7j1Le2FkFIv42d9qolWhEizyGmCQLSSil6Ij65etAnZnLP98HNk7dDOxF1kWjHm7kmGqLUtRtMrH+m5lAEcCZ2RvRw6Treqn8L6QX5EWql97e1Mr+5geObPQ63hfBIMGZ/G+LWiygNy4vwKfL1OmSoQaQXZ7Q7AdSCVyRDQJNCHlJdkrHbcEACJBWZmpGymdhONGmQ2m442Q6R2fzMXbbIgAu/RNt4lz6Vo5kET/',
   user => 'cirode',
   type => 'ssh-rsa',
 }
